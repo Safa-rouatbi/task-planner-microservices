b17994d6c0d7db291ac1007b81e2d579c4ce78b3
Safa-rouatbi <safarouatbi999@gmail.com>
2025-07-12 10:15:30 +0100

    fixed : externalisation de la cle secrete JWT dans la config

--- planification des taches/authentification-service/src/main/java/com/example/authentification_service/security/JwtUtil.java
+++ planification des taches/authentification-service/src/main/java/com/example/authentification_service/security/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +15,9 @@ import java.util.function.Function;
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "RDPc0T93jXGgL2qDcsi+QK1v3q5L0Mn+ou2XGEhTx6Q=";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
     public Key getSigningKey() {
     byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
     return Keys.hmacShaKeyFor(keyBytes);
